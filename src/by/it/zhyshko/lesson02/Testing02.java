12436f298374ca9512a7e72241aa0c19aec4790e
Volha <volhazhyshko@mail.com>
2019-10-08 11:45:40 +0300

    lesson02

--- src/by/it/zhyshko/lesson02/Testing02.java
+++ src/by/it/zhyshko/lesson02/Testing02.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 public class Testing02 {
 
 
+
     @Test(timeout = 1500)
     public void testTaskA1() throws Exception {
         run("").include("Hello world!");
